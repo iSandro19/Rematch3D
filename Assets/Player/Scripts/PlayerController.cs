6464820b15b5bd5ec0e7907afe856ef82e21d9f1
Nicolas Vazquez Cancela <nicolas.vazquez.cancela@udc.es>
2023-04-19 23:42:17 +0200

    Añadido control por celdas con animaciones

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -2,42 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    IDLE, RUN
+}
+
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller;
-    public Animator animator;
-    public Vector3 velocity;
-    public float maxVelocity = 5f;
-    public float gravityScale = 0.01f;
-    public float angle = 0f;
+    public Animator anim;
+
+    public float tileW = 1f;
+    public float tileH = 1f;
+    public float runTime = 0f;
+    public float runTimeMax = 0.4f;
+    public Vector3 originPos, destPos;
+    public Quaternion originAng, destAng;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-        velocity = new Vector3(0f, 0f, 0f);
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (controller.isGrounded)
-            velocity.y = 0;
-        else
-            velocity.y += Physics.gravity.y * gravityScale;
+        switch ((PlayerState)anim.GetInteger("state"))
+        {
+            case PlayerState.IDLE:
 
-        velocity.x = Input.GetAxis("Horizontal") * maxVelocity;
-        velocity.z = Input.GetAxis("Vertical") * maxVelocity;
-        angle = 90f - Mathf.Rad2Deg * Mathf.Atan2(velocity.z, velocity.x);
+                if (Input.GetAxisRaw("Horizontal") != 0f)
+                {
+                    float dir = Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+
+                    anim.SetInteger("state", (int)PlayerState.RUN);
+
+                    originPos = transform.position;
+                    destPos = transform.position;
+                    destPos.x += dir * tileW;
+
+                    originAng = transform.rotation;
+                    destAng = Quaternion.AngleAxis(
+                        90f * dir,
+                        Vector3.up
+                    );
+
+                }
+                else if (Input.GetAxisRaw("Vertical") != 0f)
+                {
+                    float dir = Mathf.Sign(Input.GetAxisRaw("Vertical"));
+
+                    anim.SetInteger("state", (int)PlayerState.RUN);
+                    originPos = transform.position;
+                    destPos = transform.position;
+                    destPos.z += dir * tileH;
+
+                    originAng = transform.rotation;
+                    destAng = Quaternion.AngleAxis(
+                       dir > 0f? 0f: 180f,
+                        Vector3.up
+                    );
+                }
+                break;
+
+            case PlayerState.RUN:
+                if (runTime < runTimeMax)
+                {
+                    runTime += Time.deltaTime;
+                    transform.position = Vector3.Lerp(originPos, destPos, runTime / runTimeMax);
+                    transform.rotation = Quaternion.Lerp(originAng, destAng, runTime / runTimeMax);
+                }
+                else
+                {
+                    runTime = 0f;
+                    anim.SetInteger("state", (int)PlayerState.IDLE);
+                }
+                break;
+        }
+        /*
+        vel.x = Input.GetAxisRaw("Horizontal") * maxVelocity;
+        vel.z = Input.GetAxis("Vertical") * maxVelocity;
+        angle = 90f - Mathf.Rad2Deg * Mathf.Atan2(vel.z, vel.x);
         transform.rotation = Quaternion.AngleAxis(
             angle,
             Vector3.up
-        );
-
-        animator.SetFloat(
-            "velocity",
-            Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical"))
-        );
-
-        controller.Move(velocity * Time.deltaTime);
+        );*/
     }
 }

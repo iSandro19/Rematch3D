97f91f76acc94e35bc7fdd84b2cc125eab1e9af3
Nicolas Vazquez Cancela <nicolas.vazquez.cancela@udc.es>
2023-04-20 21:54:20 +0200

    Modificado movimiento del jugador

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -4,86 +4,55 @@ using UnityEngine;
 
 public enum PlayerState
 {
-    IDLE, RUN
+    IDLE, RUN, ATK
 }
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController charac;
     public Animator anim;
-
-    public float tileW = 1f;
-    public float tileH = 1f;
-    public float runTime = 0f;
-    public float runTimeMax = 0.4f;
-    public Vector3 originPos, destPos;
-    public Quaternion originAng, destAng;
+    public Vector3 vel;
+    public float velMax = 20f;
+    public float atkTime = 0f;
+    public float atkCooldown = 0.4f; 
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+        charac = GetComponent< CharacterController >();
+        anim = GetComponent< Animator >();
     }
 
     void Update()
     {
-        switch ((PlayerState)anim.GetInteger("state"))
+        if (atkTime <= 0f)
         {
-            case PlayerState.IDLE:
+            vel.x = Input.GetAxis("Horizontal") * velMax;
+            vel.z = Input.GetAxis("Vertical") * velMax;
 
-                if (Input.GetAxisRaw("Horizontal") != 0f)
-                {
-                    float dir = Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+            if (vel == Vector3.zero)
+            {
+                anim.SetInteger("state", (int)PlayerState.IDLE);
+            }
+            else
+            {
+                transform.rotation = Quaternion.AngleAxis(
+                    90f - Mathf.Rad2Deg * Mathf.Atan2(vel.z, vel.x),
+                    Vector3.up
+                );
+                anim.SetInteger("state", (int)PlayerState.RUN);
 
-                    anim.SetInteger("state", (int)PlayerState.RUN);
+                charac.Move(vel * Time.deltaTime);
+            }
 
-                    originPos = transform.position;
-                    destPos = transform.position;
-                    destPos.x += dir * tileW;
-
-                    originAng = transform.rotation;
-                    destAng = Quaternion.AngleAxis(
-                        90f * dir,
-                        Vector3.up
-                    );
-
-                }
-                else if (Input.GetAxisRaw("Vertical") != 0f)
-                {
-                    float dir = Mathf.Sign(Input.GetAxisRaw("Vertical"));
-
-                    anim.SetInteger("state", (int)PlayerState.RUN);
-                    originPos = transform.position;
-                    destPos = transform.position;
-                    destPos.z += dir * tileH;
-
-                    originAng = transform.rotation;
-                    destAng = Quaternion.AngleAxis(
-                       dir > 0f? 0f: 180f,
-                        Vector3.up
-                    );
-                }
-                break;
-
-            case PlayerState.RUN:
-                if (runTime < runTimeMax)
-                {
-                    runTime += Time.deltaTime;
-                    transform.position = Vector3.Lerp(originPos, destPos, runTime / runTimeMax);
-                    transform.rotation = Quaternion.Lerp(originAng, destAng, runTime / runTimeMax);
-                }
-                else
-                {
-                    runTime = 0f;
-                    anim.SetInteger("state", (int)PlayerState.IDLE);
-                }
-                break;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                atkTime = atkCooldown;
+                anim.Play("ATK", -1, 0);
+            }
         }
-        /*
-        vel.x = Input.GetAxisRaw("Horizontal") * maxVelocity;
-        vel.z = Input.GetAxis("Vertical") * maxVelocity;
-        angle = 90f - Mathf.Rad2Deg * Mathf.Atan2(vel.z, vel.x);
-        transform.rotation = Quaternion.AngleAxis(
-            angle,
-            Vector3.up
-        );*/
+        else
+        {
+            atkTime -= Time.deltaTime;
+        }
     }
 }

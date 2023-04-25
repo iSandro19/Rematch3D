6e816a0d893d58520c0b30b3f4919687f674eb99
Óscar Alejandro Manteiga Seoane <manteigaseoaneoscar@gmail.com>
2023-04-25 18:58:03 +0200

    Update PawnController.cs

--- Assets/Pieces/Pawn/Scripts/PawnController.cs
+++ Assets/Pieces/Pawn/Scripts/PawnController.cs
@@ -7,41 +7,39 @@ public class PawnController : ObjEnemy
     public MeshRenderer rend;
     public float moveSpeed = 2f; // Velocidad de movimiento del enemigo
     public float jumpDistance = 2f; // Distancia del salto del enemigo
-    public int mapSize = 10; // Tamaño del mapa
-    private bool hasMovedThisTurn = false; // Variable para llevar registro si el enemigo ya se movió en el turno actual
 
     void Start()
     {
         life = 3;
         invincible = false;
-        dmgCooldown = 1f;
-        dmgTime = 0f;
+        dmgCooldown = 0.2f;
+        dmgTime = 0.2f;
         rend = GetComponent<MeshRenderer>();
 
-        // Iniciar la corrutina para mover el objeto cada 3 segundos
-        StartCoroutine(MoveObjectEvery3Seconds());
+        // Iniciar la corrutina para mover el objeto cada 1 segundo
+        StartCoroutine(MoveObjectEvery1Second());
     }
 
-    IEnumerator MoveObjectEvery3Seconds()
+    IEnumerator MoveObjectEvery1Second()
     {
+        float stepSize = 1f; // Tamaño de cada paso
+
         while (true)
         {
-            // Esperar 3 segundos
-            yield return new WaitForSeconds(3f);
+            // Seleccionar una dirección aleatoria (0, 90, 180 o 270 grados)
+            float angle = Random.Range(0, 4) * 90f;
 
-            // Si el objeto no está muerto ni invencible, moverlo a una posición aleatoria
-            if (!dead && !invincible && !hasMovedThisTurn)
+            // Calcular la dirección del movimiento en función del ángulo
+            Vector3 dir = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+            // Comprobar si la dirección elegida es válida (no sale del mapa y no colisiona con ningún objeto)
+            RaycastHit hit;
+            float newX = transform.position.x + dir.x * stepSize;
+            float newZ = transform.position.z + dir.z * stepSize;
+            if (!Physics.Raycast(transform.position, dir, out hit, stepSize))
             {
-                // Generar un ángulo aleatorio en grados
-                float angle = Random.Range(0f, 360f);
-                // Convertir el ángulo a una dirección en el plano XZ
-                Vector2 direction = new Vector2(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad));
-                // Calcular la posición objetivo
-                Vector3 targetPos = transform.position + new Vector3(direction.x, 0f, direction.y) * jumpDistance;
-                // Asegurarse de que el enemigo no salga del área del mapa
-                targetPos.x = Mathf.Clamp(targetPos.x, -mapSize + 0.5f, mapSize - 0.5f);
-                targetPos.z = Mathf.Clamp(targetPos.z, -mapSize + 0.5f, mapSize - 0.5f);
-                // Mover el enemigo progresivamente al punto de destino
+                // Mover gradualmente hacia la nueva posición
+                Vector3 targetPos = transform.position + dir * stepSize;
                 float t = 0f;
                 while (t < 1f)
                 {
@@ -49,19 +47,10 @@ public class PawnController : ObjEnemy
                     transform.position = Vector3.Lerp(transform.position, targetPos, t);
                     yield return null;
                 }
-                // Asegurarse de que el enemigo no haya chocado con ningún objeto en el camino
-                Collider[] hits = Physics.OverlapSphere(transform.position, 0.5f);
-                foreach (Collider hit in hits)
-                {
-                    if (hit.gameObject != gameObject && hit.isTrigger)
-                    {
-                        transform.position = transform.position - new Vector3(direction.x, 0f, direction.y) * jumpDistance;
-                        break;
-                    }
-                }
-                // Actualizar la variable que indica si el enemigo ya se movió en el turno actual
-                hasMovedThisTurn = true;
             }
+
+            // Esperar 1 segundo antes de volver a mover el objeto
+            yield return new WaitForSeconds(1f);
         }
     }
 
@@ -94,17 +83,5 @@ public class PawnController : ObjEnemy
     protected override void OnAtk(int dmg)
     {
         base.OnAtk(dmg);
-    }
-
-    // Evento que se ejecuta al inicio de cada turno
-    public void OnTurnStart()
-    {
-        hasMovedThisTurn = true;
-    }
-
-    // Evento que se ejecuta al final de cada turno
-    public void OnTurnEnd()
-    {
-        hasMovedThisTurn = false;
     }
 }
